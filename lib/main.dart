ab5198f013e985b82a9061f80414010e0d3c99e9
Jordhan <jordhan.rdz@gmail.com>
2019-12-20 18:07:29 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import './screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Deli Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
